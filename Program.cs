dc2f544592ff74d5dbfd66b60b3d75de22dd03f7
KevinEshaghi <113677462+KevinEshaghi@users.noreply.github.com>
2025-05-30 18:46:14 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,20 @@
-using bankapi.Data;
-using bankapi.Server;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace bankapi
+namespace BankBlazor.Client;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7208") });
 
-            builder.Services.AddScoped<IAccountService, AccountService>();
-            builder.Services.AddScoped<ICustomerService, CustomerService>();
 
-            builder.Services.AddDbContext<BankBlazorContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowBlazor", builder =>
-                {
-                    builder.WithOrigins("https://localhost:7249") 
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                });
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("AllowBlazor");
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
-        }
+        await builder.Build().RunAsync();
     }
 }
